e6183c0228c118ca7223c3b4f69913717d904667
VanceXie <vance-xie@outlook.com>
2023-03-18 04:32:06 +0800

    初始化

--- ReName.py
+++ ReName.py
@@ -1,16 +1,16 @@
 import os
+import random
+import re
+import string
 
 
 def rename_by_order(path: str, extension: str):
-    # # 指定目录路径
-    # path = "path/to/directory"
-    
     # 列出目录下所有文件
     files = os.listdir(path)
     
     # 按照文件名排序
-    files.sort(key=lambda x: int(x.split('.')[0][3:-1]))
-    
+    # files.sort(key=lambda x: int(x.split('.')))
+    files.sort(key=lambda l: int(re.findall('\d+', l)[0]))  # 找出字符串中的数字并依据其整形进行排序
     # 初始化计数器
     count = 1
     
@@ -25,3 +25,60 @@ def rename_by_order(path: str, extension: str):
         os.rename(src, dst)
         # 计数器加一
         count += 1
+
+
+def rename_by_size(path: str):
+    # 列出目录下所有文件
+    files = os.listdir(path)
+    
+    # 获取文件大小并按照文件大小排序
+    files.sort(key=lambda x: os.path.getsize(os.path.join(path, x)))
+    
+    # 初始化计数器
+    count = 1
+    
+    # 遍历文件并重命名
+    for file in files:
+        __filename, extension = os.path.splitext(file)
+        
+        new_name = str(count) + extension
+        
+        # 如果新文件名已存在，则在文件名后面添加数字
+        j = 1
+        while os.path.exists(os.path.join(path, new_name)):
+            # 构造新文件名
+            new_name = f'{count}_{j}.{extension[1:]}'
+            j += 1
+        # 拼接路径和文件名
+        src = os.path.join(path, file)
+        dst = os.path.join(path, new_name)
+        # 重命名文件
+        os.rename(src, dst)
+        # 计数器加一
+        count += 1
+
+
+def rename_by_type(path: str, condition):
+    # 遍历目录
+    for root, dirs, files in os.walk(path):
+        # 获取文件列表，并按文件大小排序
+        files = sorted(files, key=condition)
+        
+        # 遍历文件
+        for i, file in enumerate(files):
+            # 获取文件类型和路径
+            file_type = file.split('.')[-1].lower()
+            file_path = os.path.join(root, file)
+            
+            # 获取文件名前缀和新文件名
+            prefix = file_type
+            new_file_name = f'{prefix}_{i + 1}.{file_type}'
+            
+            # 如果新文件名已存在，则在文件名后面添加数字
+            j = 1
+            while os.path.exists(os.path.join(root, new_file_name)):
+                new_file_name = f'{prefix}_{i + 1}_{j}.{file_type}'
+                j += 1
+            
+            # 重命名文件
+            os.rename(file_path, os.path.join(root, new_file_name))
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import ReName
 
-
-
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    ReName.rename_by_order(r'D:\vance\Downloads\[全彩国漫] 白蛇&小狐仙&雏蜂，等“稀有”国漫H同人 “OOOP”1-5\OOOP-05', 'jpg')
+    ReName.rename_by_order(r'D:\vance\Downloads\白蛇&小狐仙&雏蜂，等“稀有”国漫H同人 “OOOP”1-5\OOOP-01', 'jpg')
+    # ReName.rename_by_size(r'D:\vance\Downloads\白蛇&小狐仙&雏蜂，等“稀有”国漫H同人 “OOOP”1-5\OOOP-01')

872eb88337eda5188f710eef3f0a5e07c9c4d932
VanceXie <vance-xie@outlook.com>
2023-04-08 01:41:40 +0800

    function movefile

--- FileOperate/DropDuplicates.py
+++ FileOperate/DropDuplicates.py
@@ -1,6 +1,21 @@
+import datetime
 import os
 import hashlib
+import shutil
+import time
+
 from tools import *
+
+
+def movefile(srcfile, dstpath):  # 移动函数
+	if not os.path.isfile(srcfile):
+		print("%s not exist!" % (srcfile))
+	else:
+		fpath, fname = os.path.split(srcfile)  # 分离文件名和路径
+		if not os.path.exists(dstpath):
+			os.makedirs(dstpath)  # 创建路径
+		shutil.move(srcfile, dstpath + fname)  # 移动文件
+		print("move %s -> %s" % (srcfile, dstpath + fname))
 
 
 def group_files_by_size(directory):
@@ -19,11 +34,12 @@ def group_files_by_size(directory):
 
 
 @print_time
-def remove_duplicates(path: str, flag: int = 0):
+def remove_duplicates(path: str, compare_flag: int = 0, delete_flag: int = 0):
 	"""
 	:param path: 去重路径
-	:param flag:0，default，通过使用MD5散列值比较文件，耗费资源更多，比较更为严格；1，通过hash值比较文件，计算较快，稳定性相对较弱
+	:param compare_flag:0，default，通过使用MD5散列值比较文件，耗费资源更多，比较更为严格；1，通过hash值比较文件，计算较快，稳定性相对较弱
 	:return: unique_files列表
+	:param delete_flag: 是否直接删除重复文件。default 0，则不删除仅集中放入“duplicates_datetime”文件夹；1，直接删除
 	"""
 	
 	# Step 2: Group the files by size
@@ -36,15 +52,18 @@ def remove_duplicates(path: str, flag: int = 0):
 			unique_filenames = set()
 			for filename in filenames:
 				with open(os.path.join(path, filename), 'rb') as f:
-					if flag == 0:
+					if compare_flag == 0:
 						file_hash = hashlib.md5(f.read()).hexdigest()
-					elif flag == 1:
+					elif compare_flag == 1:
 						file_hash = hash(f.read())
 				if file_hash not in unique_filenames:
 					unique_filenames.add(file_hash)
 				else:
 					filenames.remove(filename)
-					os.remove(filename)
+					if delete_flag == 1:
+						os.remove(filename)  # 删除文件
+					else:
+						movefile(os.path.join(path, filename), os.path.join(path, 'duplicates_' + datetime.datetime.now().strftime('%Y%m%d_%H:%M:%S')))  # 移动文件
 			file_sizes[size] = filenames
 	
 	# Step 5: Merge all the remaining files into a single list
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from FileOperate import DropDuplicates, ReName
 # import FileOperate
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-	path = r'D:\vance\Pictures\电子光圈'
+	path = r'D:\vance\Downloads\MollyRedWolf'
 	DropDuplicates.remove_duplicates(path, 0)
 	# ReName.rename_by_num(path, 0, '0', 3)
 	

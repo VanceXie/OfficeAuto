0f647c20a50e43919c868b91089bcfaefc111519
VanceXie <vance-xie@outlook.com>
2023-05-27 19:23:56 +0800

    关键字图片爬虫

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from FileOperate.Achieve import multithread_winrar_compress
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-	path = r"D:\vance\Music"
-	# DropDuplicates.remove_duplicates(path, 0, 1)
-	# ReName.rename_by_sort(path, 150, '0', 4)
-	multithread_winrar_compress(path, 2, 128)
+	path = r"D:\Fenkx\Fenkx - General\AI\Dataset\满箱扫码"
+	DropDuplicates.remove_duplicates(path, 0, 1)
+	# ReName.rename_by_sort(path, 1, '0', 3)
+	# multithread_winrar_compress(path, 2, 128)

d344dc0e460de054ea6dcb0a5ff5ec43d8207d54
VanceXie <vance-xie@outlook.com>
2023-06-14 01:38:39 +0800

    图片相似度计算

--- FileOperate/DropDuplicates.py
+++ FileOperate/DropDuplicates.py
@@ -4,21 +4,21 @@ import os
 import shutil
 
 import numpy as np
+from numpy import linalg
 
 from tools import calculate_time
-import cv2
 
 
-def movefile(srcfile, dstdir):  # 移动函数
+def movefile(srcfile, dst_dir):  # 移动函数
 	if not os.path.isfile(srcfile):
-		print("%s not exist!" % (srcfile))
+		print("%s not exist!" % srcfile)
 	else:
-		fpath, fname = os.path.split(srcfile)  # 分离文件名和路径
-		if not os.path.exists(dstdir):
-			os.makedirs(dstdir)  # 创建路径
-		dsypath = os.path.join(dstdir, fname)
-		shutil.move(srcfile, dsypath)  # 移动文件
-		print("move %s -> %s" % (srcfile, dsypath))
+		filepath, filename = os.path.split(srcfile)  # 分离文件名和路径
+		if not os.path.exists(dst_dir):
+			os.makedirs(dst_dir)  # 创建路径
+		dst_path = os.path.join(dst_dir, filename)
+		shutil.move(srcfile, dst_path)  # 移动文件
+		print("move %s -> %s" % (srcfile, dst_path))
 
 
 def group_files_by_size(directory):
@@ -81,21 +81,6 @@ def remove_duplicates(path: str, compare_flag: int = 0, delete_flag: int = 0):
 	return unique_files
 
 
-# 余弦相似度计算
-from PIL import Image
-from numpy import average, dot, linalg
-
-
-# 对图片进行统一化处理
-def get_thum(image, size=(64, 64), greyscale=False):
-	# 利用image对图像大小重新设置, Image.ANTIALIAS为高质量的
-	image = image.resize(size, Image.Resampling.LANCZOS)
-	if greyscale:
-		# 将图片转换为L模式，其为灰度图，其每个像素用8个bit表示
-		image = image.convert('L')
-	return image
-
-
 # 计算图片的余弦距离
 def image_similarity_vectors(images):
 	"""
@@ -110,62 +95,3 @@ def image_similarity_vectors(images):
 	# dot返回的是点积，对二维数组（矩阵）进行计算
 	res = np.dot(images_norms, images_norms.T)
 	return corr_coef, res
-
-
-# image1 = Image.open(r'D:\vance\Downloads\知一妹妹\pics\0234.jpg')
-# image2 = Image.open(r'D:\vance\Downloads\知一妹妹\pics\0235.jpg')
-# cosin = image_similarity_vectors(image1, image2)
-# print('图片余弦相似度', cosin)
-def find_similar_images(folder_path, size: tuple):
-	images_path = ([os.path.join(folder_path, filename) for filename in os.listdir(folder_path) if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png")])
-	# Use the images_path list to find similar images
-	similar_images = []
-	images = [(cv2.resize((cv2.imdecode(np.fromfile(image_path, np.uint8), 0)), size)).ravel() for image_path in images_path]
-	corr_coef, res = image_similarity_vectors(images)
-	
-	for i in range(len(images_path)):
-		for j in range(i + 1, len(images_path)):
-			similarity = image_similarity_vectors(Image.open(images_path[i]), Image.open(images_path[j]))
-			if similarity > 0.995:
-				# Add the similar images to a list
-				similar_images.append((images_path[i], images_path[j]))
-	# Remove duplicate images from the list of similar images
-	unique_images = list(set(sum(similar_images, ())))
-	# Remove all but one image from each similar image group
-	for i in range(len(unique_images)):
-		for j in range(i + 1, len(unique_images)):
-			if unique_images[i] in unique_images[j]:
-				unique_images[j] = unique_images[j].replace(unique_images[i], "")
-	# Remove empty strings from the list of similar images
-	unique_images = [x for x in unique_images if x != ""]
-	# Group similar images together
-	similar_groups = []
-	for image in unique_images:
-		group = [x for x in unique_images if x.startswith(image)]
-		if len(group) > 1:
-			similar_groups.append(group)
-	# Remove all but one image from each similar image group
-	unique_groups = []
-	for group in similar_groups:
-		unique_groups.append((group[0],))
-	# Convert the list of unique image groups to a list of similar image pairs
-	similar_images = []
-	for group in unique_groups:
-		for i in range(len(group)):
-			for j in range(i + 1, len(group)):
-				similar_images.append((group[i], group[j]))
-	return similar_images
-
-
-def delete_similar_images(similar_images):
-	for image_pair in similar_images:
-		for image_path in image_pair:
-			os.remove(image_path)
-	print("Similar images deleted successfully.")
-
-
-# # Example usage
-# folder_path = r"D:\vance\Downloads\知一妹妹\pics"
-# similar_images = find_similar_images(folder_path, (64, 64))
-# print(similar_images)
-# delete_similar_images(similar_images)
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from FileOperate.Achieve import multithread_winrar_compress
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-	path = r"D:\Fenkx\Fenkx - General\AI\Dataset\BarCode\My Datasets\Test_Label\C10"
+	path = r"D:\ProgramData\Temp\BP3730-2\L N"
 	DropDuplicates.remove_duplicates(path, 1, 1)
-	pre = os.path.basename(path)+'_'
-	ReName.rename_by_sort(path, 1, prefix=pre, fill_char='0', length=4)
-# multithread_winrar_compress(path, 2, 128)
+	# # pre = os.path.basename(path)+'_'
+	# ReName.rename_by_sort(path, 1, prefix='', fill_char='0', length=4)
+	# multithread_winrar_compress(path, 1, 64)
